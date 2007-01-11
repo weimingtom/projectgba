4bafa81dcd830869576e4796e4218d2b08fab907
evilgeniuscow <evilgeniuscow@cc8b292c-bc26-0410-82bf-d7f40f438c40>
2007-01-11 06:42:36 +0000

    Lots new in this version. Now it parses several directives, parses numbers in different bases, and calculates label addresses, among other things.

--- trunk/ProjectGBA/Assembler/MainForm.cs
+++ trunk/ProjectGBA/Assembler/MainForm.cs
@@ -10,6 +10,7 @@ using System;
 using System.Collections.Generic;
 using System.Drawing;
 using System.Windows.Forms;
+using System.Text.RegularExpressions;
 
 namespace ProjectGBA
 {
@@ -43,16 +44,79 @@ namespace ProjectGBA
 		
 		#endregion
 		
+		int parseInt(string str) {
+			string[] prefixes = new string[] {"0x","#0x","$","%"};	//prefixes
+			int[] bases = new int[] {16, 16, 16, 2};				//corresponding bases
+			for(int i = 0; i < prefixes.Length; ++i) {
+				if(str.StartsWith(prefixes[i]))
+					return Convert.ToInt32(str.Substring(prefixes[i].Length), bases[i]);
+			}
+			string[] suffixes = new string[] {"h","d","b"};		//suffixes
+			bases = new int[] {16, 10, 2};						//corresponding bases
+			for(int i = 0; i < suffixes.Length; ++i) {
+				if(str.EndsWith(suffixes[i]))
+					return Convert.ToInt32(str.Substring(0,str.LastIndexOf(suffixes[i])), bases[i]);
+			}
+			return Convert.ToInt32(str, 10); //if no prefix or suffix, assume decimal
+		}
+		
 		void BAssembleClick(object sender, EventArgs e)
 		{
+			int address = 0;			//keeps track of address, etc.
+			
 			string[] lines = tCode.Text.Split(new string[] {Environment.NewLine}, StringSplitOptions.None);
 			foreach(string line in lines) {
 				string tmp = line;
 				int c = tmp.IndexOfAny(comment);		//only accepts chars, so can't do C-style comments like this
 				if(c != -1)
-					tmp = tmp.Substring(0, c);		//strip comments
-				tmp = tmp.Trim(whitespace);
-				System.Diagnostics.Trace.WriteLine(tmp);
+					tmp = tmp.Substring(0, c);			//strip comments
+				tmp = tmp.Trim();						//defaults to stripping whitespace
+				tmp = Regex.Replace(tmp, @"\s+", " ");	//replace long whitespace to single space
+				tmp = Regex.Replace(tmp, @"\s*,\s*", ","); //remove space around commas (for splitting)
+				if(tmp != "") {
+					if(tmp.StartsWith("@")) {			//directive
+						string[] args = tmp.Split(new char[] {' '});
+						switch(args[0]) {
+							case "@arm":
+								System.Diagnostics.Trace.WriteLine("== ARM Mode ==");
+								break;
+							case "@dcd":
+								System.Diagnostics.Trace.WriteLine("Data: " + args[1]);
+								break;
+							case "@echo":
+								string[] strs = args[1].Split(new char[] {','});
+								foreach(string str in strs) {
+									System.Diagnostics.Trace.WriteLine("Echo:" + str);
+								}
+								break;
+							case "@endarea":
+								System.Diagnostics.Trace.WriteLine("== End Area ==");
+								break;
+							case "@textarea":
+								System.Diagnostics.Trace.WriteLine("== Area: " + args[1] + " ==");
+								address = parseInt(args[1]);
+								break;
+							case "@thumb":
+								System.Diagnostics.Trace.WriteLine("== THUMB Mode ==");
+								break;
+							default:
+								System.Diagnostics.Trace.WriteLine("Directive: " + args[0].Substring(1));
+								break;
+						}
+					} else {
+						string[] args = tmp.Split(new char[] {' '});
+						if(args.Length > 1) {
+							switch(args[0]) {
+								default:
+									System.Diagnostics.Trace.WriteLine("OPCODE: " + args[0]);
+									break;
+							}
+							address += 2; //each opcode is 2 bytes? (except for bl?)
+						} else {
+							System.Diagnostics.Trace.WriteLine("Label: " + args[0] + " = 0x" + Convert.ToString(address, 16));
+						}
+					}
+				}
 			}
 		}
 		
