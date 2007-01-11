6b6a8b49e2990cb2d548d647c886d953046ff75e
evilgeniuscow <evilgeniuscow@cc8b292c-bc26-0410-82bf-d7f40f438c40>
2007-01-11 04:43:29 +0000

    Nothing really yet, just some simple whitespace removal.

---
+++ MainForm.Designer.cs
@@ -0,0 +1,77 @@
+/*
+ * Created by SharpDevelop.
+ * User: Spikeman
+ * Date: 1/10/2007
+ * Time: 7:08 PM
+ * 
+ */
+namespace ProjectGBA
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.tCode = new System.Windows.Forms.TextBox();
+			this.bAssemble = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// tCode
+			// 
+			this.tCode.AcceptsTab = true;
+			this.tCode.Location = new System.Drawing.Point(12, 12);
+			this.tCode.MaxLength = 10000000;
+			this.tCode.Multiline = true;
+			this.tCode.Name = "tCode";
+			this.tCode.Size = new System.Drawing.Size(268, 220);
+			this.tCode.TabIndex = 0;
+			// 
+			// bAssemble
+			// 
+			this.bAssemble.Location = new System.Drawing.Point(12, 238);
+			this.bAssemble.Name = "bAssemble";
+			this.bAssemble.Size = new System.Drawing.Size(75, 23);
+			this.bAssemble.TabIndex = 1;
+			this.bAssemble.Text = "Assemble";
+			this.bAssemble.UseVisualStyleBackColor = true;
+			this.bAssemble.Click += new System.EventHandler(this.BAssembleClick);
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(292, 273);
+			this.Controls.Add(this.bAssemble);
+			this.Controls.Add(this.tCode);
+			this.Name = "MainForm";
+			this.Text = "ProjectGBA";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+		}
+		private System.Windows.Forms.Button bAssemble;
+		private System.Windows.Forms.TextBox tCode;
+	}
+}
