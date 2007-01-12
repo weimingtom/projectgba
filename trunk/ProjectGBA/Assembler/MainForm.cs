6e1787f19196fce6e5137f2cbf6f23c7509bd205
evilgeniuscow <evilgeniuscow@cc8b292c-bc26-0410-82bf-d7f40f438c40>
2007-01-12 23:28:54 +0000

    This version evaluates expressions! It's just before the point where assembly can take place. I'm also including some notes.

--- trunk/ProjectGBA/Assembler/MainForm.cs
+++ trunk/ProjectGBA/Assembler/MainForm.cs
@@ -4,9 +4,15 @@
  * Date: 1/10/2007
  * Time: 7:08 PM
  * 
+ * To Fix:
+ * - Support for =address
+ * - Add ~ (not) and - (unary minus) to expression evaluation
+ * - Make precidence matter in expressions evaluation
+ * 
  */
 
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Drawing;
 using System.Windows.Forms;
@@ -22,6 +28,8 @@ namespace ProjectGBA
 		
 		char[] whitespace = new char[] {' ', '	'};
 		char[] comment = new char[] {';'};
+		
+		Dictionary<string, string> defines = new Dictionary<string, string>();
 		
 		#endregion
 		
@@ -44,6 +52,8 @@ namespace ProjectGBA
 		
 		#endregion
 		
+		#region Parsing and Expressions
+		
 		int parseInt(string str) {
 			string[] prefixes = new string[] {"0x","#0x","$","%"};	//prefixes
 			int[] bases = new int[] {16, 16, 16, 2};				//corresponding bases
@@ -60,65 +70,230 @@ namespace ProjectGBA
 			return Convert.ToInt32(str, 10); //if no prefix or suffix, assume decimal
 		}
 		
+		bool canBeParsed(string str) {
+			try {
+				parseInt(str);
+				return true;
+			} catch {
+				return false;
+			}
+		}
+		
+		string eval(string exp) {
+			//needs precidence
+			//need to evaluate ~ and unary -
+			try {
+				return Convert.ToString(parseInt(exp)); //return parsed number if possible
+			} catch {
+				List<string> nums = new List<string>();
+				List<string> ops = new List<string>();
+				nums.AddRange(exp.Split(new string[] {"+","-","*","/",">>","<<","&","|","^","?"},
+				                        StringSplitOptions.None));
+				ops.AddRange(Regex.Split(exp, @"\d+")); //get list of operators
+				ops.RemoveAt(0);
+				ops.RemoveAt(ops.Count - 1);
+				while(ops.Count > 0) {
+					int result = 0;
+					int num0 = Convert.ToInt32(nums[0], 10);
+					int num1 = Convert.ToInt32(nums[1], 10);
+					switch(ops[0]) {
+						case "+":
+							result = num0 + num1;
+							break;
+						case "-":
+							result = num0 - num1;
+							break;
+						case "/":
+							result = num0 / num1;
+							break;
+						case "*":
+							result = num0 * num1;
+							break;
+						case ">>":
+							result = num0 >> num1;
+							break;
+						case "<<":
+							result = num0 << num1;
+							break;
+						case "&":
+							result = num0 & num1;
+							break;
+						case "|":
+							result = num0 | num1;
+							break;
+						case "^":
+							result = num0 ^ num1;
+							break;
+						case "?":							//mod?
+							result = num0 % num1;
+							break;
+					}
+					nums.RemoveAt(0);
+					nums[0] = Convert.ToString(result);	//replace first two with result
+					ops.RemoveAt(0);
+				}
+				return nums[0];
+			}
+		}
+		
+		string evalExp(string str) {
+			MatchEvaluator matchEval = new MatchEvaluator(this.matchEval);
+			//regular expression finds number, matchEval puts them into decimal
+			string decstr = Regex.Replace(str, @"\#*(0x)*\$*\%*[0-9a-fA-F]+[hdb]*", matchEval);
+			int idx;
+			while((idx=decstr.IndexOf(')')) != -1) {
+				int idx2 = decstr.LastIndexOf('(', idx);
+				decstr = decstr.Substring(0, idx2) + eval(decstr.Substring(idx2 + 1, idx - idx2 - 1))
+					+ decstr.Substring(idx + 1);
+			}
+			return eval(decstr);
+		}
+		
+		string matchEval(Match m) {
+			return  Convert.ToString(parseInt(m.Value));
+		}
+		
+		#endregion
+		
 		void BAssembleClick(object sender, EventArgs e)
 		{
 			int address = 0;			//keeps track of address, etc.
 			
 			string[] lines = tCode.Text.Split(new string[] {Environment.NewLine}, StringSplitOptions.None);
-			foreach(string line in lines) {
-				string tmp = line;
-				int c = tmp.IndexOfAny(comment);		//only accepts chars, so can't do C-style comments like this
-				if(c != -1)
-					tmp = tmp.Substring(0, c);			//strip comments
-				tmp = tmp.Trim();						//defaults to stripping whitespace
-				tmp = Regex.Replace(tmp, @"\s+", " ");	//replace long whitespace to single space
-				tmp = Regex.Replace(tmp, @"\s*,\s*", ","); //remove space around commas (for splitting)
-				if(tmp != "") {
-					if(tmp.StartsWith("@")) {			//directive
-						string[] args = tmp.Split(new char[] {' '});
-						switch(args[0]) {
-							case "@arm":
-								System.Diagnostics.Trace.WriteLine("== ARM Mode ==");
-								break;
-							case "@dcd":
-								System.Diagnostics.Trace.WriteLine("Data: " + args[1]);
-								break;
-							case "@echo":
-								string[] strs = args[1].Split(new char[] {','});
-								foreach(string str in strs) {
-									System.Diagnostics.Trace.WriteLine("Echo:" + str);
-								}
-								break;
-							case "@endarea":
-								System.Diagnostics.Trace.WriteLine("== End Area ==");
-								break;
-							case "@textarea":
-								System.Diagnostics.Trace.WriteLine("== Area: " + args[1] + " ==");
-								address = parseInt(args[1]);
-								break;
-							case "@thumb":
-								System.Diagnostics.Trace.WriteLine("== THUMB Mode ==");
-								break;
-							default:
-								System.Diagnostics.Trace.WriteLine("Directive: " + args[0].Substring(1));
-								break;
-						}
-					} else {
-						string[] args = tmp.Split(new char[] {' '});
-						if(args.Length > 1) {
+			ArrayList nextLines = new ArrayList();
+			
+			// The first pass(es) processes whitespace, comments, and all defines, labels, etc.
+			#region First Pass
+			
+			bool needsToBeParsed; //keep track if everything is parsed
+			int numPasses = 0;		//for debug, counts passes
+			do {
+				needsToBeParsed = false;
+				foreach(string line in lines) {
+					bool addLine = true;	//keeps track of if to keep line (for removing defines and labels)
+					string tmp = line;
+					foreach(KeyValuePair<string, string> kvp in defines) {
+						tmp = tmp.Replace(kvp.Key, kvp.Value);	//replace defines
+					}
+					int c = tmp.IndexOfAny(comment);		//only accepts chars, so can't do C-style comments like this
+					if(c != -1)
+						tmp = tmp.Substring(0, c);			//strip comments
+					tmp = tmp.Trim();						//defaults to stripping whitespace
+					tmp = Regex.Replace(tmp, @"\s+", " ");	//replace long whitespace to single space
+					tmp = Regex.Replace(tmp, @"\s*,\s*", ","); //remove space around commas (for splitting)
+					if(tmp != "") {
+						if(tmp.StartsWith("@")) {			//directive
+							string[] args = tmp.Split(new char[] {' '});
 							switch(args[0]) {
+								case "@arm":
+									System.Diagnostics.Trace.WriteLine("== ARM Mode ==");
+									break;
+								case "@dcd":
+									System.Diagnostics.Trace.WriteLine("Data: " + args[1]);
+									if(!canBeParsed(args[1]))
+										needsToBeParsed = true;
+									address += 4;
+									break;
+								case "@define":
+									System.Diagnostics.Trace.WriteLine("Define Added: " + args[1] + " = " + args[2]);
+									defines.Add(args[1], args[2]);
+									addLine = false;
+									break;
+								case "@echo":
+									string[] strs = args[1].Split(new char[] {','});
+									foreach(string str in strs) {
+										System.Diagnostics.Trace.WriteLine("Echo:" + str);
+									}
+									break;
+								case "@endarea":
+									System.Diagnostics.Trace.WriteLine("== End Area ==");
+									break;
+								case "@textarea":
+									System.Diagnostics.Trace.WriteLine("== Area: " + args[1] + " ==");
+									address = parseInt(args[1]);
+									break;
+								case "@thumb":
+									System.Diagnostics.Trace.WriteLine("== THUMB Mode ==");
+									break;
 								default:
-									System.Diagnostics.Trace.WriteLine("OPCODE: " + args[0]);
+									System.Diagnostics.Trace.WriteLine("Directive: " + args[0].Substring(1));
 									break;
 							}
-							address += 2; //each opcode is 2 bytes? (except for bl?)
 						} else {
-							System.Diagnostics.Trace.WriteLine("Label: " + args[0] + " = 0x" + Convert.ToString(address, 16));
+							string[] args = tmp.Split(new char[] {' '});
+							if(args.Length > 1) {
+								System.Diagnostics.Trace.WriteLine("OPCODE: " + args[0]);
+								address += 2; //each opcode is 2 bytes? (except for bl?)
+							} else {
+								string addressStr = "0x" + Convert.ToString(address, 16);
+								System.Diagnostics.Trace.WriteLine("Label: " + args[0] + " = " + addressStr);
+								defines.Add(args[0], addressStr);
+								addLine = false;
+							}
 						}
+					} else {
+						addLine = false; //remove empty lines
 					}
+					if(addLine)
+						nextLines.Add(tmp);
 				}
+				lines = new string[nextLines.Count];
+				nextLines.CopyTo(lines);
+				nextLines.Clear();
+				++numPasses;
+				System.Diagnostics.Trace.WriteLine("\n==== End of Pass " + numPasses.ToString() + " ====\n");
+			} while(needsToBeParsed);
+			
+			#endregion
+			
+			// The second pass evaluates expressions in arguments
+			#region Second Pass
+			
+			foreach(string line in lines) {
+				string tmp = line;
+				if(!tmp.StartsWith("@")) {
+				   	string[] parts = tmp.Split(' ');
+					string[] args = Regex.Split(parts[1], @"(?![\[\{]{1}[^,]*),(?![^,\[\{]*[\]\}]{1})"); //match commas not inside braces
+					for(int i = 0; i < args.Length; ++i) {
+						if(args[i].StartsWith("[")) {
+							string[] subargs = args[i].Substring(1, args[1].Length - 2).Split(',');
+							
+							for(int j = 0; j < subargs.Length; ++j) {
+								System.Diagnostics.Trace.WriteLine(subargs[j]);
+								if(!Regex.IsMatch(subargs[j], @"r{1}\d{1,2}"))
+									subargs[j] = evalExp(subargs[j]);
+							}
+							args[i] = "[" + String.Join(",",subargs) + "]";
+						}
+						if(!Regex.IsMatch(args[i], @"r{1}\d{1,2}")) //only if not r0-15
+						   args[i] = evalExp(args[i]);
+					}
+					parts[1] = String.Join(",",args);
+					tmp = String.Join(" ", parts);
+				}
+				nextLines.Add(tmp);
 			}
+			nextLines.CopyTo(lines);
+			nextLines.Clear();
+			
+			#endregion
+			
+			tCode.Text = String.Join(Environment.NewLine, lines);
+			
 		}
 		
+		void BDebugClick(object sender, EventArgs e)
+		{
+			//System.Diagnostics.Trace.WriteLine(evalExp("0x8fcef40+2"));
+		}
+		
+		void MainFormLoad(object sender, EventArgs e)
+		{
+			{	//everything has these defines
+				defines.Add("pc","r15");
+				defines.Add("lr","r14");
+				defines.Add("sp","r13");
+			}
+		}
 	}
 }
